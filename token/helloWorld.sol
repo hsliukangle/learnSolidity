1b4c6a15e87f556457d56a3e3be997e10d18b267
mason <hsliukangle@163.com>
2021-09-26 10:40:18 +0800

    hello world

---
+++ token/helloWorld.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.4;
+
+contract HelloWorld {
+    string message = "hello world";
+    
+    function setMessage(string memory _message) public {
+        message = _message;
+    }
+    
+    function getMessage() public view returns (string memory) {
+        return message;
+    }
+}
